0ff5e0a3244199628f51eb8083659c5e9142feb5
kholifah <tm.kholifah@gmail.com>
2015-01-15 16:22:22 +0700

    new version

--- login.php
+++ login.php
@@ -3,15 +3,15 @@
 <head>
 	<meta charset="UTF-8">
 	<title>welcome to chechalk</title>
-	<link rel="stylesheet" href="assets/font/web%2520fonts/roboto_regular_macroman/stylesheet.css">
+	<link rel="stylesheet" href="assets/font/web-fonts/roboto_regular_macroman/stylesheet.css">
 	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="assets/css/normalize.css">
 	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 	<div class="login-form">
-		<ul class="login-tab-menu">
-			<li><a href="#login" class="active">Login</a></li>
+		<ul class="login-tab-menu clear">
+			<li class="active"><a href="#login" class="active">Login</a></li>
 			<li><a href="#register">Register</a></li>
 		</ul>
 		
