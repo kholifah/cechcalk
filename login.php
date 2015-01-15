9e4073d01a9c99adc7205258a2de94a460984027
kholifah <tm.kholifah@gmail.com>
2015-01-15 11:06:20 +0700

    styling and add scss

--- login.php
+++ login.php
@@ -9,9 +9,9 @@
 	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-	<div class="form-login">
-		<ul>
-			<li><a href="#login">Login</a></li>
+	<div class="login-form">
+		<ul class="login-tab-menu">
+			<li><a href="#login" class="active">Login</a></li>
 			<li><a href="#register">Register</a></li>
 		</ul>
 		
